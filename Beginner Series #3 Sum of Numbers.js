b759c123ff1e8aab76f8eec36ad3a680a1826de4
Flex <endlessxxxpain@gmail.com>
2023-01-09 18:57:06 +0300

    2

--- Beginner Series #3 Sum of Numbers.js
+++ Beginner Series #3 Sum of Numbers.js
@@ -1,4 +1,29 @@
 function getSum( a,b )
 {
-   
+  let sum = 0;
+  
+   if(a < b)
+     {
+       
+       for(let i = a; i <= b; i++)
+      {
+       sum += i;
+      }
+       
+      }
+       
+      else if (a > b)
+       {
+          for(let i = b; i <= a; i++)
+        {
+          sum += i;
+        } 
+         
+       }
+      else
+       {
+          return a + "since both are same";
+       }
+
+  return sum;
 }
