ebcf75f2080bee19a2eaf35c6f1b9edc8fada66d
Flex <endlessxxxpain@gmail.com>
2023-01-11 00:50:02 +0300

    2

--- Basic JS - Calculating averages.js
+++ Basic JS - Calculating averages.js
@@ -1,7 +1,14 @@
 var Calculator = 
-{ 
+{
  average: function() 
  {
-  
+  var s = 0;
+
+  for(var i = 0; i < arguments.length; i++)
+  {
+    s += parseInt(arguments[i]);
+  }
+
+  return arguments.length === 0 ? 0 : s / arguments.length;
  }
 };
