497d59a3b205a8f3230b962313d4b62b69290d44
Flex <endlessxxxpain@gmail.com>
2023-01-11 01:25:00 +0300

    2

--- Find Maximum and Minimum Values of a List.js
+++ Find Maximum and Minimum Values of a List.js
@@ -1,9 +1,29 @@
-var min = function(list){
-    
-    return list[0];
+var min = function(list)
+{
+  let min = list[0];
+  
+    for(let i = 0; i < list.length; i++)
+      {
+        if(min >= list[i])
+          {
+            min = list[i]
+          }
+      }
+  
+    return  min;
 }
 
-var max = function(list){
-    
-    return list[0];
+var max = function(list)
+{
+  let max = list[0];
+  
+    for(let i = 0; i < list.length; i++)
+      {
+          if(max <= list[i])
+          {
+            max = list[i]
+          }
+      }
+  
+    return  max;
 }
